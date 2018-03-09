162c291f363ce8ee2ac526736d0b99ff9e4f8b5b
Theara Seng <seng.theara.kh@gmail.com>
2018-03-09 14:32:14 +0700

    create UserData to match the JSON

---
+++ src/main/java/com/theara/restful/domain/UserData.java
@@ -0,0 +1,21 @@
+package com.theara.restful.domain;
+
+import java.util.List;
+
+/**
+ * @author Theara Seng
+ * Created on Mar 09, 2018
+ */
+
+public class UserData {
+
+    private List<User> data;
+
+    public List<User> getData() {
+        return data;
+    }
+
+    public void setData(List<User> data) {
+        this.data = data;
+    }
+}
