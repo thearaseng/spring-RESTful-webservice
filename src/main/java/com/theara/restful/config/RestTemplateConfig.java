3f3d7580ff4673ce8ffeaa7391ec778bff714875
Theara Seng <seng.theara.kh@gmail.com>
2018-03-09 14:29:41 +0700

    RestTemplate config

---
+++ src/main/java/com/theara/restful/config/RestTemplateConfig.java
@@ -0,0 +1,21 @@
+package com.theara.restful.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Theara Seng
+ * Created on Mar 09, 2018
+ */
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
+    }
+
+}
